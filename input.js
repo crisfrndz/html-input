fb7e0a64f667af54233906055681f540ea21d5dc
crisfrndz <114977127+crisfrndz@users.noreply.github.com>
2023-09-01 20:01:14 +0800

    Add files via upload

---
+++ input.js
@@ -0,0 +1,19 @@
+function display() {
+  var fname = document.getElementById("fname").value;
+  var mname = document.getElementById("mname").value;
+  var lname = document.getElementById("lname").value;
+  var bday = document.getElementById("bday").value;
+  var address = document.getElementById("address").value;
+  alert(
+    "Name: " +
+      fname +
+      " " +
+      mname +
+      " " +
+      lname +
+      "\nBirthday: " +
+      bday +
+      "\nAddress: " +
+      address
+  );
+}
